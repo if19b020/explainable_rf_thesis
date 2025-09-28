11fd8ca2482cd054f68be941099c8d2739c6dbae
if19b020 <if19b020@technikum-wien.at>
2025-09-28 15:29:56 +0200

    added class_counts

--- src/node.py
+++ src/node.py
@@ -2,10 +2,10 @@ class Node:
     def __init__(self, feature=None, threshold=None, left=None, right=None, value=None, class_counts=None):
         self.feature = feature
         self.threshold = threshold
-        self.left = left # child node
-        self.right = right # child node
-        self.value = value # only for leaf nodes
-        self.class_counts = class_counts # for feature contribution
+        self.left_child = left 
+        self.right_child = right
+        self.value = value
+        self.class_counts = class_counts
         
     def is_leaf_node(self):
         return self.value is not None
